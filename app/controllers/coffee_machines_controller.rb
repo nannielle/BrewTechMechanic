df8c71979f432916aea48dd35324854f9743480f
Arash Rasoulian <arashrasoulian1@gmail.com>
2024-03-12 21:23:05 +0100

    add env file

--- app/controllers/coffee_machines_controller.rb
+++ app/controllers/coffee_machines_controller.rb
@@ -6,4 +6,9 @@ class CoffeeMachinesController < ApplicationController
   def show
     @coffee_machine = CoffeeMachine.find(params[:id])
   end
+
+  def my_coffeemachine
+    @user_coffeemachines = CoffeeMachine.where(user_id: current_user.id)
+    @user = current_user
+  end
 end
